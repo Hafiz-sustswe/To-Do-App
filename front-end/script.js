20bb818abdb722071745a9d141ef79047f30a0fc
Md Sadman Hafiz <hafiz.sust333@gmail.com>
2023-07-24 08:08:41 +0600

    Some modification and finalise for the presentation

--- front-end/script.js
+++ front-end/script.js
@@ -1,54 +1,50 @@
-// Function to toggle between sign-up and login forms
 function toggleForms() {
-  const registerForm = document.getElementById("signup-form");
+  const signupForm = document.getElementById("signup-form");
   const loginForm = document.getElementById("login-form");
 
-  if (registerForm.classList.contains("hidden")) {
-    // If sign-up form is hidden, show it and hide login form
-    registerForm.classList.remove("hidden");
-    loginForm.classList.add("hidden");
+  if (signupForm.style.display === "none") {
+    signupForm.style.display = "block";
+    loginForm.style.display = "none";
   } else {
-    // If sign-up form is visible, hide it and show login form
-    registerForm.classList.add("hidden");
-    loginForm.classList.remove("hidden");
+    signupForm.style.display = "none";
+    loginForm.style.display = "block";
   }
 }
 
-// Function to handle user registration
-function register(event) {
+function signup(event) {
   event.preventDefault();
 
   // Get input values
-  const fullName = document.getElementById("fullName").value;
+  const name = document.getElementById("name").value;
   const gender = document.getElementById("gender").value;
-  const role = document.getElementById("role").value;
+  const designation = document.getElementById("designation").value;
   const phoneNumber = document.getElementById("phoneNumber").value;
   const password = document.getElementById("password").value;
 
-  // Create registration data object
-  const registerData = {
-    fullName: fullName,
+  // Create signup request body
+  const signupData = {
+    name: name,
     gender: gender,
-    role: role,
+    designation: designation,
     phoneNumber: phoneNumber,
     password: password,
   };
 
-  // Make a POST request to register user
-  fetch("http://localhost:8000/api/v1/auth/register", {
+  // Make a POST request to the signup API
+  fetch("http://localhost:8000/api/v1/auth/signup", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(registerData),
+    body: JSON.stringify(signupData),
   })
     .then((response) => response.json())
     .then((data) => {
+      // Handle signup response
       if (data.success) {
-        // Hide registration form and show login form
-        document.getElementById("register-form").style.display = "none";
+        // Redirect to login page
+        document.getElementById("signup-form").style.display = "none";
         document.getElementById("login-form").style.display = "block";
-        
       } else {
         // Show error message
         alert(data.message);
@@ -56,7 +52,7 @@ function register(event) {
     })
     .catch((error) => {
       console.error("Error:", error);
-      alert("Something went wrong while registering. Please try again.");
+      alert("An error occurred during signup. Please try again.");
     });
 }
 
@@ -153,6 +149,8 @@ function createTodo(event) {
 }
 
 // Function to fetch and display the user's todo list
+
+
 function fetchTodoList(accessToken) {
   // Make a GET request to fetch the todo list
   fetch("http://localhost:8000/api/v1/todos", {
@@ -163,34 +161,26 @@ function fetchTodoList(accessToken) {
     .then((response) => response.json())
     .then((data) => {
       if (data.success) {
-        // Clear existing todo list
-
-       
-
         const todoList = document.getElementById("todo-items");
         todoList.innerHTML = "Tasks To Be Done :";
 
         // Display user's todo list
         const todoItems = data.data;
-        let i = 1;
-        todoItems.forEach((item) => {
-          // Create list item for each todo
-          
+        todoItems.forEach((item, index) => {
           const li = document.createElement("li");
           li.classList.add("todo-item");
-          li.textContent = "Task " + i + " : " + item.title  + "    ";
-          i = i+1 ;
+          li.textContent = "Task " + (index + 1) + " : " + item.title + "    ";
+
           // Create description element
           const description = document.createElement("div");
-          description.classList.add("todo-item-des"); 
+          description.classList.add("todo-item-des");
           description.textContent = "Description : " + item.description + "    ";
-          
 
           // Create edit button for each todo
           const editButton = document.createElement("button");
           editButton.textContent = "Edit";
           editButton.classList.add("edit-button");
-          editButton.addEventListener("click", () => editTodoItem(item.id));
+          editButton.addEventListener("click", () => editTodoItem(index));
           li.appendChild(editButton);
 
           // Create delete button for each todo
@@ -212,9 +202,6 @@ function fetchTodoList(accessToken) {
           // Append todo item to the list
           todoList.appendChild(li);
           todoList.appendChild(description);
-
-
-          
         });
       } else {
         // Show error message
@@ -223,11 +210,12 @@ function fetchTodoList(accessToken) {
     })
     .catch((error) => {
       console.error("Error:", error);
-      alert(
-        "Something went wrong while fetching the todo list. Please try again."
-      );
+      alert("Something went wrong while fetching the todo list. Please try again.");
     });
 }
+
+
+
 
 // Function to update the status of a todo
 function updateTodoStatus(todoId, isDone) {
@@ -293,27 +281,35 @@ function deleteTodoItem(todoId) {
 }
 
 // Function to edit a todo
+// Function to edit a todo
 function editTodoItem(todoId) {
   // Get access token from localStorage
   const accessToken = localStorage.getItem("accessToken");
 
   // Get the todo item element
-  const todoItemElement = document.getElementById(todoId);
+  const todoItem = document.getElementById("todo-" + todoId);
+
+  if (!todoItem) {
+    alert("Todo not found.");
+    return;
+  }
 
   // Get the title and description elements of the todo item
-  const titleElement = todoItemElement.querySelector(".title");
-  const descriptionElement = todoItemElement.querySelector(".description");
+  const titleElement = todoItem.querySelector(".todo-item");
+  const descriptionElement = todoItem.querySelector(".todo-item-des");
+
+  if (!titleElement || !descriptionElement) {
+    alert("Todo item is not properly formatted.");
+    return;
+  }
 
   // Get the current title and description values
-  const currentTitle = titleElement.textContent;
-  const currentDescription = descriptionElement.textContent;
+  const currentTitle = titleElement.textContent.split(":")[1].trim();
+  const currentDescription = descriptionElement.textContent.split(":")[1].trim();
 
   // Prompt the user to enter the new title and description
   const newTitle = prompt("Enter the new title:", currentTitle);
-  const newDescription = prompt(
-    "Enter the new description:",
-    currentDescription
-  );
+  const newDescription = prompt("Enter the new description:", currentDescription);
 
   // If the user clicked Cancel or entered empty values, do nothing
   if (newTitle === null || newDescription === null || newTitle === "" || newDescription === "") {
